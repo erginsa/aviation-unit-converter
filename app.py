36ad4da067c297d9cd4a75ccc89c864d06e024b4
Ergin <ergin.sabanci@gmail.com>
2025-05-12 20:02:21 +0300

    fixing reset function

--- app.py
+++ app.py
@@ -177,8 +177,10 @@ with colA:
     convert_clicked = st.button("Convert", key="convert_button")
 with colB:
     if st.button("Reset"):
-        for key in list(st.session_state.keys()):
-            del st.session_state[key]
+        reset_keys = ["from_unit", "to_unit", "input_value", "history"]
+        for key in reset_keys:
+            if key in st.session_state:
+                del st.session_state[key]
         st.experimental_rerun()
 
 # Convert Logic
